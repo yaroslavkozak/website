71fab7ef3e67d985efc7d175e13f20983a7804cd
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-01 02:02:42 +0300

    finish bd setup

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 //echo "config file included";
   define("BASE_URL","/");
 	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] );
-	define("DB_HOST","mysql");
-	define("DB_NAME","internet-programming-spring-classroom-mairako");
+	define("DB_HOST","localhost");
+	define("DB_NAME","yrslvkzk_project");
 	define("DB_PORT","3306");
-	define("DB_USER","mairako");
-    define("DB_PASS","SsjzKN4ypJb6YEuk");
+	define("DB_USER","yrslv_kzk");
+    define("DB_PASS","Koling094835");
     
