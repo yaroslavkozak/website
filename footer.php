6df1c7a7160db9222ae3e4fe2f7620a338b2ac4a
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 01:17:47 +0300

    corrected

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <!-- Footer -->
 
-    <footer class="footer">
+    <footer class="footer-section">
         <hr />
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
