af84c666e50d983a7a2141609b491f75affe45a6
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 01:58:24 +0300

    test

--- portfolio.php
+++ portfolio.php
@@ -152,8 +152,9 @@ include('header.php');
         <!-- PORTFOLIO -->
     <!-- <link href="/portfolio/ionicons/css/ionicons.min.css" rel="stylesheet"> -->
     <!-- !TODO ICONS -->
-    <link href="/portfolio/style.css" rel="stylesheet">
-    <script src="/portfolio/modernizr.js"></script>
+    
+    <!-- <link href="/portfolio/style.css" rel="stylesheet"> -->
+    <script src="/portfolio/modernizr.js"></script> 
 
 
     <!-- jQuery -->
