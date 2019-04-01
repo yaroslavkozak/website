2c7ba8dd6b53d0f08f7bdb42e5e0579b16192fbb
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 01:50:40 +0300

    disable style + icons

--- portfolio.php
+++ portfolio.php
@@ -150,9 +150,9 @@ include('header.php');
 
 
         <!-- PORTFOLIO -->
-    <link href="/portfolio/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <!-- <link href="/portfolio/ionicons/css/ionicons.min.css" rel="stylesheet"> -->
     <!-- !TODO ICONS -->
-    <link href="/portfolio/style.css" rel="stylesheet">
+    <!-- <link href="/portfolio/style.css" rel="stylesheet"> -->
     <script src="/portfolio/modernizr.js"></script>
 
 
