966881415b5ee17a134f4a2ac660ffd944710769
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 02:32:59 +0300

    ready

--- portfolio/custom.js
+++ portfolio/custom.js
@@ -51,3 +51,13 @@ $(window).load(function () {
         }, scroll_top_duration);
     });
 
+    // input
+    $(".input-contact input, .textarea-contact textarea").focus(function () {
+        $(this).next("span").addClass("active");
+    });
+    $(".input-contact input, .textarea-contact textarea").blur(function () {
+        if ($(this).val() === "") {
+            $(this).next("span").removeClass("active");
+        }
+    });
+});
