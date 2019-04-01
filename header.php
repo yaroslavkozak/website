f8ccaa6527dbc0b6e2cd5c1df20d41afd577fcd6
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 02:04:50 +0300

    testing

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 
 
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/css/custom.css" />
+    <link rel="stylesheet" href="css/custom.css" />
   </head>
 
   <body>
