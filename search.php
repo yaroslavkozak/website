c9ac7579c26242efcdf3a6dfd37301a9e0a36bf0
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 17:22:29 +0300

    test

--- search.php
+++ search.php
@@ -11,6 +11,10 @@ error_reporting(E_ALL);
 
 
 <h1>Search</h1>
+
+
+
+
 <?php
     $query = $_GET['search']; 
     // gets value sent over search form
@@ -39,33 +43,9 @@ error_reporting(E_ALL);
           }
           
       ?>
-      
-        //  means that it selects all fields, you can also write: `id`, `title`, `text`
-        // articles is the name of our table
-         
-        // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-        // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
-        // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
-         
-        if(mysql_num_rows($results) > 0){ // if one or more rows are returned do following
-             
-            while($results = mysql_fetch_array($results)){
-            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-             
-                <!-- echo "<p><h3>".$results['title']."</h3>".$results['title']."</p>"; -->
-                // posts results gotten from database(title and text) you can also show id ($results['id'])
-            }
-             
-        }
-        else{ // if there is no matching rows do following
-            echo "No results";
-        }
-         
-    // }
-    // else{ // if query length is less than minimum
-    //     echo "Minimum length is ".$min_length;
-    // }
-?>
+
+
+
 
 
 <?php
