05c78baf8218e885313ea85f6252b597c5c9d729
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 17:25:16 +0300

    test

--- header.php
+++ header.php
@@ -99,7 +99,7 @@
         </ul>
 
         <form class="form-inline mt-2 mt-md-0" action="search.php" method="GET" >
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" name="search" >
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" name="query" >
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
           </form>
 
