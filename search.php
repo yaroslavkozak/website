5c84a99f735d1c46c3345bd3381a8cd35e5edb8d
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 17:15:37 +0300

    test

--- search.php
+++ search.php
@@ -38,9 +38,9 @@ error_reporting(E_ALL);
         // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
         // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
          
-        if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+        if(mysql_num_rows($results) > 0){ // if one or more rows are returned do following
              
-            while($results = mysql_fetch_array($raw_results)){
+            while($results = mysql_fetch_array($results)){
             // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
              
                 echo "<p><h3>".$results['title']."</h3>".$results['title']."</p>";
