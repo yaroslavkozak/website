32182c42abfb315d83baac551c8fa268a144e7a5
yaroslavkozak <yrslvkzk@gmail.com>
2019-04-02 15:55:13 +0300

    about

--- about.php
+++ about.php
@@ -17,9 +17,9 @@ include('header.php');
         <div class="col-lg-7">
           <div class="about-descr">
 
-            <p class="p-heading">Born and raised in Mumbai, India, I’m currently finishing my final year of Bachelor In Design program at York University/Sheridan College (also known as YSDN), in Toronto, Canada. I am a product design student, who loves crafting big ideas and is passionate about crafting meaningful experiences that influence positive change and can help make the world a better place. </p>
-            <p class="separator">Driven by the idea that people can be motivated to do good, I aim to use creative problem-solving and human-centred design approach to create efficient and dynamic digital experiences that are accessible and inclusive for everyone to enjoy. At my core, I believes that in order to craft a seamless user experience and bring innovative ideas to life, it is important to understand the intersection of design, business, technology and the user.</p>
-            <p class="separator">I am a self-motivated designer who is always looking for new challenges and opportunities that can help me continue to develop and grow as a designer. I’m actively seeking full-time new grad opportunities for 2019!
+            <p class="p-heading">I'm Yaroslav.Web Developer,IT Specialist,Tech Enthusiast,Musician,Philosopherand a Dreamerfrom Ukraine(currently, living and working in Greece)</p>
+            <p class="separator">Driven by the idea that people can be motivated to do good, I aim to use creative problem-solving and human-centred solutions for efficient and dynamic digital experiences that are accessible and inclusive for everyone to enjoy. At my core, I believes that in order to craft a seamless user experience and bring innovative ideas to life, it is important to understand the intersection of design, business, technology and the user.</p>
+            <p class="separator">I am a self-motivated freelance web-developer who is always looking for new challenges and opportunities that can help me continue to develop and grow as a professional. I’m actively seeking full-time new grad opportunities for 2019!
 
 Thanks for viewing my portfolio! 🙏</p>
 
